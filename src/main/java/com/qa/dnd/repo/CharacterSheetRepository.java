918f394357ff3321cbdb21fca134c0da2d1a5853
Chrisctr <credfern96@gmail.com>
2020-05-04 15:01:49 +0100

    Trying to get Postman APIs to work with backend still still

--- src/main/java/com/qa/dnd/repo/CharacterSheetRepository.java
+++ src/main/java/com/qa/dnd/repo/CharacterSheetRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CharacterSheetRepository extends JpaRepository<CharacterSheet, Long> {
 
-    CharacterSheet findByTitle(String title);
-
 }
