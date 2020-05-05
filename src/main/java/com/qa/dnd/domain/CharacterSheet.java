382938a09f2c1c1c5843d2a42c49759bbd9b2f27
Chrisctr <credfern96@gmail.com>
2020-05-05 20:29:54 +0100

    Deleted redundant variables from CharacterSheet classes due to revised ERD

--- src/main/java/com/qa/dnd/domain/CharacterSheet.java
+++ src/main/java/com/qa/dnd/domain/CharacterSheet.java
@@ -1,26 +1,19 @@
 package com.qa.dnd.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "CHARACTERSHEET")
 public class CharacterSheet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String character_name;
-    private String class_name;
     private Long max_hp;
     private Long current_hp;
-    private int hit_dice;
-    private int level;
-    private int speed;
-    private String weapon_training;
-    private String armour_training;
+    private Long exp;
 
     public CharacterSheet() {
     }
@@ -33,36 +26,12 @@ public class CharacterSheet {
         this.character_name = character_name;
     }
 
-    public void setClass_name(String class_name) {
-        this.class_name = class_name;
-    }
-
     public void setMax_hp(Long max_hp) {
         this.max_hp = max_hp;
     }
 
     public void setCurrent_hp(Long current_hp) {
         this.current_hp = current_hp;
-    }
-
-    public void setHit_dice(int hit_dice) {
-        this.hit_dice = hit_dice;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public void setWeapon_training(String weapon_training) {
-        this.weapon_training = weapon_training;
-    }
-
-    public void setArmour_training(String armour_training) {
-        this.armour_training = armour_training;
     }
 
     public Long getId() {
@@ -73,10 +42,6 @@ public class CharacterSheet {
         return character_name;
     }
 
-    public String getClass_name() {
-        return class_name;
-    }
-
     public Long getMax_hp() {
         return max_hp;
     }
@@ -85,44 +50,22 @@ public class CharacterSheet {
         return current_hp;
     }
 
-    public int getHit_dice() {
-        return hit_dice;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-    public String getWeapon_training() {
-        return weapon_training;
-    }
-
-    public String getArmour_training() {
-        return armour_training;
-    }
-
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CharacterSheet)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof CharacterSheet))
+            return false;
         CharacterSheet that = (CharacterSheet) o;
-        return getHit_dice () == that.getHit_dice () &&
-                getCharacter_name () == that.getCharacter_name () &&
-                getLevel () == that.getLevel () &&
-                getSpeed () == that.getSpeed () &&
-                getId ().equals (that.getId ()) &&
-                getClass_name ().equals (that.getClass_name ()) &&
+        return getId ().equals (that.getId ()) &&
+                getCharacter_name ().equals (that.getCharacter_name ()) &&
                 getMax_hp ().equals (that.getMax_hp ()) &&
                 getCurrent_hp ().equals (that.getCurrent_hp ()) &&
-                getWeapon_training ().equals (that.getWeapon_training ()) &&
-                getArmour_training ().equals (that.getArmour_training ());
+                exp.equals (that.exp);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash (getId (), getCharacter_name(), getClass_name (), getMax_hp (), getCurrent_hp (), getHit_dice (), getLevel (), getSpeed (), getWeapon_training (), getArmour_training ());
+        return Objects.hash (getId (), getCharacter_name (), getMax_hp (), getCurrent_hp (), exp);
     }
 }
