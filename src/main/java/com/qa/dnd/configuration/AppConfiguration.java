65da69cf6b7b1792ba41d1a110dffd28bfa48119
Chrisctr <credfern96@gmail.com>
2020-05-06 14:05:46 +0100

    Added more tests to CharacterSheetServiceUnitTest

---
+++ src/main/java/com/qa/dnd/configuration/AppConfiguration.java
@@ -0,0 +1,16 @@
+package com.qa.dnd.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
--- src/test/java/com/qa/dnd/service/CharacterSheetServiceUnitTest.java
+++ src/test/java/com/qa/dnd/service/CharacterSheetServiceUnitTest.java
@@ -1,13 +1,53 @@
 package com.qa.dnd.service;
 
+import com.qa.dnd.domain.CharacterSheet;
+import com.qa.dnd.repo.CharacterSheetRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class CharacterSheetServiceUnitTest {
 
-    
+    @InjectMocks
+    private CharacterSheetService service;
 
+    @Mock
+    private CharacterSheetRepository repository;
 
+    @Mock
+    private ModelMapper mapper;
+
+    private List<CharacterSheet> characterSheetList;
+    private CharacterSheet testCharacter;
+    private final long id = 1L;
+    private CharacterSheet testCharacterSheetWithID;
+
+    @Before
+    public void setUp() {
+        this.characterSheetList = new ArrayList<>();
+        this.testCharacter = new CharacterSheet();
+        this.characterSheetList.add(testCharacter);
+        this.testCharacterSheetWithID = new CharacterSheet();
+        this.testCharacterSheetWithID.setId(id);
+    }
+
+    @Test
+    public void getAllCharacterSheetsTest() {
+        when(repository.findAll()).thenReturn(this.characterSheetList);
+        ///DTO test stuff goes here
+        assertFalse("Service returned no character sheets", this.service.readCharacterSheet().isEmpty());
+        verify(repository, times(1)).findAll();
+    }
 
 }
