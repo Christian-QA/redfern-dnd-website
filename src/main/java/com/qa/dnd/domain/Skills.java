1b5d641de40ae0111e6881f54220ac9647190bb7
Chrisctr <credfern96@gmail.com>
2020-05-07 18:29:14 +0100

    Creating new branch for figuring out Spring-GCP link

--- src/main/java/com/qa/dnd/domain/Skills.java
+++ src/main/java/com/qa/dnd/domain/Skills.java
@@ -15,7 +15,37 @@ public class Skills {
     @MapsId
     private CharacterSheet characterSheet;
 
+    public Skills() {
+    }
 
+    public Skills(String skillName, CharacterSheet characterSheet) {
+        SkillName = skillName;
+        this.characterSheet = characterSheet;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setSkillName(String skillName) {
+        SkillName = skillName;
+    }
+
+    public void setCharacterSheet(CharacterSheet characterSheet) {
+        this.characterSheet = characterSheet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSkillName() {
+        return SkillName;
+    }
+
+    public CharacterSheet getCharacterSheet() {
+        return characterSheet;
+    }
 
 
 }
