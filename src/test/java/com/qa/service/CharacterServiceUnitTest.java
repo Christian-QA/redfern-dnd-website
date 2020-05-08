14407c64e3b0aa411685910631395ae7b8a5f669
Chrisctr <credfern96@gmail.com>
2020-05-08 22:48:19 +0100

    More Unit Tests for Character

--- src/test/java/com/qa/service/CharacterServiceUnitTest.java
+++ src/test/java/com/qa/service/CharacterServiceUnitTest.java
@@ -2,6 +2,7 @@ package com.qa.service;
 
 import com.qa.domain.Character;
 import com.qa.dto.CharacterDTO;
+import com.qa.exceptions.CharacterNotFoundException;
 import com.qa.repo.CharacterRepo;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.*;
 
@@ -59,5 +61,36 @@ public class CharacterServiceUnitTest {
         when(this.mapper.map(testCharacterWithID, CharacterDTO.class)).thenReturn(characterDTO);
         assertFalse("Service returned no Notes", this.service.readCharacter().isEmpty());
         verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    public void createCharacterTest(){
+        when(repository.save(testCharacter)).thenReturn(testCharacterWithID);
+        when(this.mapper.map(testCharacterWithID, CharacterDTO.class)).thenReturn(characterDTO);
+        assertEquals(this.service.createCharacter (testCharacter), this.characterDTO);
+        verify(repository, times(1)).save(this.testCharacter);
+    }
+
+    @Test
+    public void findCharacterByIdTest(){
+        when(this.repository.findById(id)).thenReturn(java.util.Optional.ofNullable(testCharacterWithID));
+        when(this.mapper.map(testCharacterWithID, CharacterDTO.class)).thenReturn(characterDTO);
+        assertEquals(this.service.findCharacterById (this.id), characterDTO);
+        verify(repository, times(1)).findById(id);
+    }
+
+    @Test
+    public void deleteCharacterByExistingId(){
+        when(this.repository.existsById(id)).thenReturn(true, false);
+        assertFalse(service.deleteCharacter(id));
+        verify(repository, times(1)).deleteById(id);
+        verify(repository, times(2)).existsById(id);
+    }
+
+    @Test(expected = CharacterNotFoundException.class)
+    public void deleteCharacterByNonExistingId(){
+        when(this.repository.existsById(id)).thenReturn(false);
+        service.deleteCharacter (id);
+        verify(repository, times(1)).existsById(id);
     }
 }
