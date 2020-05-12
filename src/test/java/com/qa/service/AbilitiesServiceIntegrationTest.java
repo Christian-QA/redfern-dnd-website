33f8ef157a97fc7f62858ac379458bd925648468
Chrisctr <credfern96@gmail.com>
2020-05-12 23:28:46 +0100

    Created Integration tests for AbilitiesService

---
+++ src/test/java/com/qa/service/AbilitiesServiceIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.qa.service;
+
+import com.qa.domain.Abilities;
+import com.qa.domain.Skills;
+import com.qa.dto.AbilitiesDTO;
+import com.qa.dto.SkillsDTO;
+import com.qa.repo.AbilitiesRepo;
+import com.qa.repo.SkillsRepo;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbilitiesServiceIntegrationTest {
+
+    @Autowired
+    private AbilitiesService service;
+
+    @Autowired
+    private AbilitiesRepo repository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    private Abilities testAbilities;
+
+    private Abilities testAbilitiesWithID;
+
+    private AbilitiesDTO mapToDTO(Abilities abilities){
+        return this.mapper.map(abilities, AbilitiesDTO.class);
+    }
+
+    @Before
+    public void setUp(){
+        this.testAbilities = new Abilities (10L, 10L, 10L, 10L, 10L, 10L);
+        this.repository.deleteAll();
+        this.testAbilitiesWithID = this.repository.save(this.testAbilities);
+    }
+
+    @Test
+    public void readAbilitiesTest(){
+        assertThat(this.service.readAbilities ())
+        .isEqualTo(
+                Stream.of(this.mapToDTO(testAbilitiesWithID)).collect(Collectors.toList())
+        );
+    }
+
+
+
+
+}
