035fb9c32a34739dd36b440af87688f99504b994
Chrisctr <credfern96@gmail.com>
2020-05-19 17:17:56 +0100

    Added favicon
    Working on selenium

---
+++ src/test/java/com/qa/mesadnd/SeleniumCharacterCRUDTest.java
@@ -0,0 +1,78 @@
+package com.qa.mesadnd;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.Thread.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class SeleniumCharacterCRUDTest {
+
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+
+    @BeforeSuite
+    public static void initializeDriver(){
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @BeforeTest
+    public void startReport(){
+        report = new ExtentReports(
+                System.getProperty("user.dir") + "/test-output/Report.html",
+                true
+        );
+        report
+                .addSystemInfo("Host Name", "QA")
+                .addSystemInfo("Tester", "Christian");
+        report.loadConfig(new File (System.getProperty("user.dir") + "\\extent-report.xml"));
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver ();
+    }
+
+    @Test
+    public void testMesaHomeTitle(){
+        test = report.startTest("Open to Mesa Home Page");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
+        driver.get("http://localhost:8181/");
+        test.log(LogStatus.INFO, "Navigating to the MesaDND homepage");
+        assertEquals(driver.getTitle(), "Mesa Home Page");
+        test.log(LogStatus.PASS, "The page name is exactly the same");
+    }
+
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test has failed " + result.getName());
+            test.log(LogStatus.FAIL, "Test has failed " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(LogStatus.PASS, "Test has passed " + result.getName());
+        }
+        report.endTest(test);
+    }
+
+    @AfterTest
+    public void endReport(){
+        report.flush();
+        report.close();
+    }
+
+
+
+}
